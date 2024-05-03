3c4a531e8942fd85c3137feded31d7eb87124642
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2024-05-02 21:39:40 -0300

    feat: TabNavigation / BottomNavigation

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,25 +1,51 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import kotlincomposemultiplatformbottomnavigation.composeapp.generated.resources.Res
-import kotlincomposemultiplatformbottomnavigation.composeapp.generated.resources.compose_multiplatform
+import tabs.home.HomeTab
+import tabs.explore.ExploreTab
+import tabs.chat.ChatTab
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-
+        TabNavigator(HomeTab){
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        TabNavigationItem(HomeTab)
+                        TabNavigationItem(ExploreTab)
+                        TabNavigationItem(ChatTab)
+                    }
+                }
+            ) {
+                CurrentTab()
+            }
+        }
     }
+}
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab){
+    val tabNavigator = LocalTabNavigator.current
+
+    BottomNavigationItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab},
+        label = { Text(tab.options.title) },
+        icon = { Icon(painter = tab.options.icon ?: rememberVectorPainter(Icons.Default.Warning), contentDescription = tab.options.title) }
+    )
 }
